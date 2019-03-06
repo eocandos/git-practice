036a9faeaa24e261aa59e29c4692e5c9a23796c2
Erick <eaocandos@gmail.com>
2019-03-05 21:45:31 -0500

    Commit one: flex example, thank you to css-tricks.com

--- GitPracticeApp/src/app/app-routing.module.ts
+++ GitPracticeApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexComponent } from '../app/flex/flex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'flex', component: FlexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GitPracticeApp/src/app/app.module.ts
+++ GitPracticeApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexComponent } from './flex/flex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlexComponent
   ],
   imports: [
     BrowserModule,
